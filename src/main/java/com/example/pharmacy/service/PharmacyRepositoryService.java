d39dbe0459485e47018f430c765114ab4d3e8798
Jyuka <yoyksks3@gmail.com>
2023-04-24 18:26:21 +0900

    @Transactional Self invocation & AOP & Proxy 학습

--- src/main/java/com/example/pharmacy/service/PharmacyRepositoryService.java
+++ src/main/java/com/example/pharmacy/service/PharmacyRepositoryService.java
@@ -6,8 +6,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -18,6 +22,37 @@ public class PharmacyRepositoryService {
 
     private final PharmacyRepository pharmacyRepository;
 
+    // self invocation test
+    public void bar(List<Pharmacy> pharmacyList) {
+        log.info("bar CurrentTransactionName: "+ TransactionSynchronizationManager.getCurrentTransactionName());
+        foo(pharmacyList);
+    }
+
+    // self invocation test
+    @Transactional
+    public void foo(List<Pharmacy> pharmacyList) {
+        log.info("foo CurrentTransactionName: "+ TransactionSynchronizationManager.getCurrentTransactionName());
+        pharmacyList.forEach(pharmacy -> {
+            pharmacyRepository.save(pharmacy);
+            throw new RuntimeException("error");
+        });
+    }
+
+
+    // read only test
+    @Transactional(readOnly = true)
+    public void startReadOnlyMethod(Long id) {
+        pharmacyRepository.findById(id).ifPresent(pharmacy ->
+                pharmacy.changePharmacyAddress("서울 특별시 광진구"));
+    }
+
+    @Transactional
+    public List<Pharmacy> saveAll(List<Pharmacy> pharmacyList) {
+        if(CollectionUtils.isEmpty(pharmacyList)) return Collections.emptyList();
+        return pharmacyRepository.saveAll(pharmacyList);
+    }
+
+    // dirty checking test
     @Transactional
     public void updateAddress(Long id, String address){
         Pharmacy entity = pharmacyRepository.findById(id).orElse(null);
@@ -29,6 +64,7 @@ public class PharmacyRepositoryService {
         entity.changePharmacyAddress(address);
     }
 
+    // dirty checking test
     public void updateAddressWithoutTransactional(Long id, String address){
         Pharmacy entity = pharmacyRepository.findById(id).orElse(null);
 
@@ -39,5 +75,10 @@ public class PharmacyRepositoryService {
         entity.changePharmacyAddress(address);
     }
 
+
+    @Transactional(readOnly = true)
+    public List<Pharmacy> findAll() {
+        return pharmacyRepository.findAll();
+    }
 
 }
