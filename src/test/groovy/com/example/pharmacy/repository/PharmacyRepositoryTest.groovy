c258f250e40e938fff21a4eedb0a84085f1a07f7
Jyuka <yoyksks3@gmail.com>
2023-04-24 16:38:08 +0900

    Spock Before/After Method 학습

--- src/test/groovy/com/example/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/example/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -9,6 +9,11 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
     @Autowired
     private PharmacyRepository pharmacyRepository;
 
+    // 각 테스트 메서드가 실행될기 전에 실행됨
+    def setup(){
+        pharmacyRepository.deleteAll()
+    }
+
     def "PharmacyRepository save"(){
         given:
         String address = "서울특별시 성북구 종암동"
@@ -30,6 +35,30 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
         result.getPharmacyName() == name
         result.getLatitude() == latitude
         result.getLongitude() == longitude
+
+    }
+
+    def "PharmacyRepository saveAll"(){
+        given:
+        String address = "서울특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        pharmacyRepository.saveAll(List.of(pharmacy))
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.size() == 1
+
+
 
     }
 }
